446db5cf35acc8a5be763bd413d299b2050975db
John Singh <wethegreenpeople@gmail.com>
2025-04-15 17:39:10 -0700

    Fix: Ingest only wasn't working

--- ingest_only.py
+++ ingest_only.py
@@ -1,13 +1,28 @@
 import asyncio
-from anytype_old import ingest_documents
+import os
+
+import nltk
+from platformdirs import user_data_dir
+from anytype import ingest_documents
+from anytype_authenticator import AnytypeAuthenticator
+from anytype_store import AnyTypeStore
 
 async def main():
-    # Optional: Run your authentication first, e.g. 
-    # await get_anytype_challenge_id()
-    # user enters secret code somewhere
-    # await get_anytype_token(secret_code)
-
     # Now run ingestion
+    persist_dir = user_data_dir("anytype-mcp", "John Singh")
+    config_path = os.path.join(persist_dir, "anytype_config.json")
+
+    auth = AnytypeAuthenticator(AnyTypeStore(None, None), config_path)
+    if auth.config.get('app_token') == None:
+        await auth.get_challenge_id()
+        print("Secret Code: ")
+        await auth.get_token(input())
+
+    nltk_dir = os.path.join(persist_dir, "nltk-data")
+    print(f"huh {nltk_dir}")
+    nltk.download("punkt_tab", download_dir=nltk_dir)
+    nltk.data.path.append(nltk_dir)
+    
     result = await ingest_documents()
     print(f"Done ingesting docs! Result: {result}")
 
